85d538c7cf1bda8aef0edb01b53858399a1e788d
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2025-05-26 07:19:46 -0700

    feat: add windows.dev.rdp

---
+++ windows.dev.rdp/setup.ps1
@@ -0,0 +1,8 @@
+winget_args = "--exact --source winget --accept-source-agreements --disable-interactivity --silent  --accept-package-agreements --force "
+
+winget.exe install --id "WireGuard.WireGuard" $winget_args
+
+
+
+
+
