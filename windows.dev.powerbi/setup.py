663a3cc548a99bcdc42828f8b6c5857925830f74
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2025-05-26 07:06:28 -0700

    env(powerbi): skip ux

--- windows.dev.powerbi/setup.py
+++ windows.dev.powerbi/setup.py
@@ -2,7 +2,7 @@ import os
 
 if __name__ == "__main__":
     os.system("scoop bucket add oughtred https://github.com/oughtred-ltd/oughtred-scoop-bucket")
-    os.system("winget install -e --id Microsoft.PowerBI")
+    os.system('winget.exe install --id "Microsoft.PowerBI" --exact --source winget --accept-source-agreements --disable-interactivity --silent  --accept-package-agreements --force')
     os.system("scoop install oughtred/bravo")
     os.system("scoop install oughtred/pbi-tools")
 
